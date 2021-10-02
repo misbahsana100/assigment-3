3498aee6ff8a57783b95e6b5e1f3b5e20d202d10
uzma maryam <uzmamaryam48@gmail.com>
2021-10-02 23:38:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,101 @@
+                                //  question no 1
+
+
+// /We have an array of cities. Assign each city in different individual variable. hint ['LA', 'new york', 'karachi','islamabad'
+
+//  let  cities = ['LA', 'new york', 'karachi','islamabad'];
+// let [LA  ,...othercities] =cities;
+
+// console.log(cities)
+                                    //  question no 2
+
+//  A function is taking 5 arguments and You have to pass it through an array
+// let arry= (fst,scnd,thrd,frth,ffth) =>{
+//  return`${fst} ,${scnd},${thrd} ,${frth} , ${ffth}`}
+// console.log(arry("1","2","3","4","5"))
+
+                                    //    question no 3
+
+// /Given an array of firstname and an array of last name. First check both array size is equal or not, if equal then make third new array which has first last name concat value
+// let firstNames = ['maryam', 'hira', 'faiza', 'aisha']
+// let lastNames = ['khan', 'ayub', 'aziz', 'yasin']
+// let firstlast =firstNames +   ''  + lastNames;
+// console.log(firstlast)
+
+                                         //    question no 4
+
+// /Create an arrow function that takes array as an input and make each element double and then return an array 
+//   const arr = [1, 2, 3,4,5,6,7,8];
+
+//   const nbr = arr.map(x => x * 2);
+                                 
+//  console.log(nbr);
+                                            //    question no 5 
+
+ //Create an arrow function that takes a number and return square of a number     
+//     let num = (c) => {  
+  
+//  let result = c*2;
+
+//   return result; 
+// };
+// console.log( num(5));
+
+                                            //   question no 6     
+//   var authenticated=true;
+//  var allowed = authenticated ? (alert('you r allowed')) :(alert('you are not allowed'))
+//            console.log(allowed)   
+           
+//         //    which alert will run
+        //     answer
+        //     allowed
+
+                                        //  question no 7
+
+        // What is the value of canDrive
+
+ // var age = 12;
+// var canDrive = age > 18 ? 'yes' : 'no';
+//  console.log(canDrive);     answer :  no
+
+                                    //    question no 8 
+
+//   let text = `He's often called "Johnny"`;
+//  console.log(text)
+//                    //answer 
+//                 //    He's often called "Johnny"
+
+                                        //  question no 9
+
+
+     //    I have called funtion twice , share the answer what is printed each time
+
+
+// function welcomeMsg(name = "Everyone") {
+//     let msg = "Welcome " + name
+//     console.log(msg)
+// }
+//  welcomeMsg("Faiza")
+//  welcomeMsg()
+                // printed
+                // Welcome Faiza
+                //  Welcome Everyone
+
+                                 //   question no 10
+
+    // make a promise that resolves after 5 sec. Use setTimeOut for resolve.
+
+
+//  function pro(){
+// return new Promise( function(resolve, reject) {
+//  setTimeout(resolve,5000,"promise is resolved")
+//    })
+//     }
+// pro()
+//  .then(mess=>{
+//         console.log(mess)
+//          })
+
+
+
+                        
